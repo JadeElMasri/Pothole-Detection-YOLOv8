92b01b0de7f687346b865caea1b245016dedc80b
JadeElMasri <jne14@mail.aub.edu>
2025-07-05 16:33:26 +0300

    Initial commit

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 import os
 
-model = YOLO(os.path.expanduser("~/Desktop/python_codes/computer_vision/object_detection/pothole_detection/runs/detect/train/weights/best.pt"))
+model = YOLO(os.path.expanduser("~/Desktop/pothole_detection/runs/detect/train/weights/best.pt"))
 
-results = model.predict(os.path.expanduser("~/Desktop/python_codes/computer_vision/object_detection/pothole_detection/Pothole.v1-raw.yolov8/test/images"),save=True,conf=0.5)
+results = model.predict(os.path.expanduser("~/Desktop/pothole_detection/Pothole.v1-raw.yolov8/test/images"),save=True,conf=0.5)
 
 
 
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
-model.train(data="/home/jade/Desktop/python_codes/computer_vision/object_detection/pothole_detection/Pothole.v1-raw.yolov8/data.yaml", epochs=50, imgsz=640)
+model.train(data="/home/jade/Desktop/pothole_detection/Pothole.v1-raw.yolov8/data.yaml", epochs=50, imgsz=640)
